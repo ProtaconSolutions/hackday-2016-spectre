34e7abb1eb2e1231e9c9d9cbbb9134245bc30989
tarlepp <tarmo.leppanen@protacon.com>
2016-12-15 21:03:53 +0200

    moar layout stuff.

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
 import { Router } from '@angular/router';
+import { LocalStorageService } from 'ng2-webstorage';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +10,23 @@ import { Router } from '@angular/router';
 })
 
 export class HeaderComponent implements OnInit {
+  public team: any;
+
   public constructor(
     private angularFire: AngularFire,
-    private router: Router
+    private router: Router,
+    private localStorage: LocalStorageService
   ) { }
 
-  public ngOnInit() {}
+  public ngOnInit() {
+    this.team = this.localStorage.retrieve('team');
+
+    this.localStorage
+      .observe('team')
+      .subscribe((value) => {
+        this.team = value;
+      });
+  }
 
   public logout() {
     this.angularFire.auth.logout();
