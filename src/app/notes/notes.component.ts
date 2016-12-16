bc34c5d8588747214d979bd9cff882127f30b1d3
tarlepp <tarmo.leppanen@protacon.com>
2016-12-16 02:40:15 +0200

    Now we can add notes!

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { LocalStorageService } from 'ng2-webstorage';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-notes',
@@ -10,6 +11,11 @@ import { LocalStorageService } from 'ng2-webstorage';
 
 export class NotesComponent implements OnInit {
   public notes: any;
+  public note: string;
+  public note2: string;
+
+  private teamKey: string;
+  private uid: string;
 
   /**
    * Constructor
@@ -20,7 +26,10 @@ export class NotesComponent implements OnInit {
   constructor(
     private angularFire: AngularFire,
     private localStorage: LocalStorageService
-  ) { }
+  ) {
+    this.teamKey = localStorage.retrieve('team').$key;
+    this.uid = localStorage.retrieve('uid');
+  }
 
   ngOnInit() {
     // TODO: Retrieve child-notes (perhaps separately) and link them to the retrieved notes.
@@ -33,8 +42,25 @@ export class NotesComponent implements OnInit {
       });
   }
 
-  private getNotesByTeamKey(teamKey)
-  {
+  public addNewNote(parent?: string) {
+    const teamKey = this.localStorage.retrieve('team').$key;
+
+    let note = {
+      parentNote: parent ? parent : '',
+      team: teamKey,
+      text: parent ? this.note2 : this.note,
+      user: this.uid,
+      createdAt: firebase.database.ServerValue.TIMESTAMP,
+      updatedAt: firebase.database.ServerValue.TIMESTAMP
+    };
+
+    this.angularFire.database.list('/notes/' + teamKey).push(note);
+
+    this.note = '';
+    this.note2 = '';
+  }
+
+  private getNotesByTeamKey(teamKey) {
     return this.angularFire.database.list('/notes/' + teamKey)
       .map(results => {
         let foo = [...results];
