43e4b700c2deef5d450acaa1b782df5d099c661e
Olli Kalmari <olli.kalmari@protacon.com>
2017-03-03 15:53:30 +0200

    Retrospective view development in progress

--- src/app/retros/retros.component.ts
+++ src/app/retros/retros.component.ts
@@ -11,6 +11,14 @@ export class RetrosComponent implements OnInit {
   public retros: FirebaseListObservable<any[]>;
   public team: any;
 
+  public madNotes: any;
+  public sadNotes: any;
+  public gladNotes: any;
+
+  public madId: string;
+  public sadId: string;
+  public gladId: string;
+
   constructor(
     private angularFire: AngularFire,
     private localStorage: LocalStorageService
@@ -19,13 +27,64 @@ export class RetrosComponent implements OnInit {
   ngOnInit() {
     this.team = this.localStorage.retrieve('team');
     this.retros = this.angularFire.database.list('/retros/'+this.team.$key);
+    this.getRetroNotes(this.team.$key);
 
     this.localStorage
       .observe('team')
       .subscribe((value) => {
         this.team = value;
         this.retros = this.angularFire.database.list('/retros/'+this.team.$key);
+
+        this.getRetroNotes(this.team.$key);
       });
   }
 
+  /**
+   * Gets retrospective notes for the given team.
+   * @param teamKey Team identifier
+   */
+  private getRetroNotes(teamKey){
+
+    let tags = this.angularFire.database.list('tags', {
+      query: {
+        orderByChild: 'type',
+        equalTo: 'MadSadGlad',
+      }
+    });
+
+    let teamNotes = this.angularFire.database.list('/notes/' + teamKey);
+
+    // Get mad notes
+    tags.map(types => types.filter(item => item.hasOwnProperty('name') && item.name === 'Mad'))
+      .subscribe(value => {
+        this.madId = value[0].$key;
+
+        teamNotes.map(notes => notes.filter(note => note.hasOwnProperty('tags') && note.tags.includes(this.madId)))
+          .subscribe(filteredNotes => {
+            this.madNotes = filteredNotes;
+          });
+      });
+
+    // Get sad notes
+    tags.map(types => types.filter(item => item.hasOwnProperty('name') && item.name === 'Sad'))
+      .subscribe(value => {
+        this.sadId = value[0].$key;
+
+        teamNotes.map(notes => notes.filter(note => note.hasOwnProperty('tags') && note.tags.includes(this.sadId)))
+          .subscribe(filteredNotes => {
+            this.sadNotes = filteredNotes;
+          });
+      });
+
+    // Get glad notes
+    tags.map(types => types.filter(item => item.hasOwnProperty('name') && item.name === 'Glad'))
+      .subscribe(value => {
+        this.gladId = value[0].$key;
+
+        teamNotes.map(notes => notes.filter(note => note.hasOwnProperty('tags') && note.tags.includes(this.gladId)))
+          .subscribe(filteredNotes => {
+            this.gladNotes = filteredNotes;
+          });
+      });
+  }
 }
