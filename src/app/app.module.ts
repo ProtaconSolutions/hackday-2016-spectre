44a45cbb49e09e0cb424ea8736e7067ebebc029f
Jussi Koutonen <jussi.koutonen@protacon.com>
2017-04-07 06:11:09 -0700

    Beautify code
    Fixed to use single quotation marks in app.module.ts and adjusted
    indentation in header.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { LoginModule } from './login/login.module';
-import { NotesModule } from './notes/notes.module'
+import { NotesModule } from './notes/notes.module';
 import { TeamsModule } from './teams/teams.module';
 import { TagsModule } from './tags/tags.module';
 import { LayoutModule } from './layout/layout.module';
-import { RetrosModule } from "./retros/retros.module";
+import { RetrosModule } from './retros/retros.module';
 import { BlankModule } from './blank/blank.module';
 import { OverviewModule } from './overview/overview.module';
-import {WarboardModule} from "./warboard/warboard.module";
+import {WarboardModule} from './warboard/warboard.module';
 
 @NgModule({
   declarations: [
